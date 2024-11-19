e8346acbca078e2ca126a8bdaff0709661ec82ba
35DR45 <104373156+35DR45@users.noreply.github.com>
2024-11-19 17:09:21 -0600

    Pantalla de progreso

--- src/components/screens/progress.jsx
+++ src/components/screens/progress.jsx
@@ -1,20 +1,30 @@
 import Footer from "../organisms/footer/footer";
 import Header from "../organisms/header/header";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import "./CSS/progress.css"
 import { Link } from "react-router-dom";
 import Progress_Card from "../organisms/progress/progress_card";
 import Profile_Button from "../atoms/perfil/profile_button";
+import { UserContext } from "../../UserContext";
 
 export default function Progress(){
-
+    const { user } = useContext(UserContext);
+    const Data={
+        usuario: user
+    }
     const [subjects, setSubjects] = useState([]);
 
     useEffect(() => {
       // Función para obtener los datos de la API
         const fetchSubjects = async () => {
         try {
-            const response = await fetch("/api/Cursos"); // Aquí va la URL de tu API
+            const response = await fetch('/api/Progreso',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(Data)
+            });; // Aquí va la URL de tu API
             const data = await response.json();
             setSubjects(data); 
         } catch (error) {
@@ -34,8 +44,8 @@ export default function Progress(){
             <div className="Course_Container">
             {subjects.map((subject, index) => (
                 <div key={index}>
-                    <p className="subject_name">{subject.NombreMateria}</p>
-                    <Progress_Card />
+                    <p className="subject_name">{subject.Materia}</p>
+                    <Progress_Card key={index} teo={subject.PTeo} pra={subject.PPra} tot={subject.PTot}/>
                 </div>
             ))}
             </div>
